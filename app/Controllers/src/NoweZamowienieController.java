a1786424af5c7de0b62431f88d87457799b0b419
armagedon444 <tomasz@mosur.net>
2018-01-08 22:49:19 +0100

    GRID VIEW IS ALIVE! controllers

--- app/Controllers/src/NoweZamowienieController.java
+++ app/Controllers/src/NoweZamowienieController.java
@@ -1,16 +1,19 @@
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Observer;
-import java.util.ResourceBundle;
+import java.time.LocalDate;
+import java.util.*;
 
 
 public class NoweZamowienieController extends ViewController implements Initializable, Observer{
@@ -54,15 +57,103 @@ public class NoweZamowienieController extends ViewController implements Initiali
     @FXML
     TextField tfRabat;
 
-    ArrayList<String> list = new ArrayList<>();
+    @FXML
+    GridPane gpPozycjeZamowienia;
+
+    ArrayList<Towar> list = new ArrayList<>();
+    LinkedList<Button> buttons = new LinkedList<>();
+
+    public void fillGridView()
+    {
+        for(int i =0; i < list.size(); i++)
+        {
+
+            //USTALANIE TESTOWYCH WARTOSCI
+            towar = list.get(i);
+            String nazwa = towar.getNazwa();
+            String cena = String.valueOf(towar.getCenaJn());
+            String ilosc = String.valueOf(i);
+            String rabat = String.valueOf(Math.random()*1).substring(0,4);
+            String wartosc = String.valueOf(i*towar.getCenaJn());
+            String dataRealizacji = LocalDate.now().toString();
+
+            Button usun = new Button("Usun");
+            Button edytuj = new Button("Edytuj");
+            edytuj.setDisable(true);
+
+                buttons.add(usun);
+                usun.setOnAction(new EventHandler<ActionEvent>() {
+                    private Button bRef;
+
+                    @Override
+                    public void handle(ActionEvent e) {
+                        usunPozycje(buttons.indexOf(bRef));
+                    }
+
+                    private EventHandler<ActionEvent> init(Button b) {
+                        bRef = b;
+                        return this;
+                    }
+                }.init(usun));
+
+
+            gpPozycjeZamowienia.addRow(i, new Label(nazwa), new Label(cena), new Label(ilosc), new Label(rabat), new Label(wartosc), new Label(dataRealizacji), usun, edytuj);
+            gpPozycjeZamowienia.getRowConstraints().add(new RowConstraints(50));
+        }
+    }
+
+    private void usunPozycje(int index)
+    {
+        buttons.clear();
+//        gpPozycjeZamowienia.getChildren().removeIf(node -> GridPane.getRowIndex(node) == index); TO-DO
+        list.remove(index);
+        gpPozycjeZamowienia.getChildren().clear();
+        fillGridView();
+    }
+
+    public void setGridViewConstraints()
+    {
+        gpPozycjeZamowienia.getColumnConstraints().get(0).setMinWidth(250);
+
+        gpPozycjeZamowienia.getColumnConstraints().get(1).setMinWidth(100);
+        gpPozycjeZamowienia.getColumnConstraints().get(1).setHalignment(HPos.CENTER);
+
+        gpPozycjeZamowienia.getColumnConstraints().get(2).setMinWidth(70);
+        gpPozycjeZamowienia.getColumnConstraints().get(2).setHalignment(HPos.CENTER);
+
+        gpPozycjeZamowienia.getColumnConstraints().get(3).setMinWidth(100);
+        gpPozycjeZamowienia.getColumnConstraints().get(3).setHalignment(HPos.CENTER);
+
+        gpPozycjeZamowienia.getColumnConstraints().get(4).setMinWidth(100);
+        gpPozycjeZamowienia.getColumnConstraints().get(4).setHalignment(HPos.CENTER);
+
+        gpPozycjeZamowienia.getColumnConstraints().get(5).setMinWidth(120);
+        gpPozycjeZamowienia.getColumnConstraints().get(5).setHalignment(HPos.CENTER);
+
+        gpPozycjeZamowienia.getColumnConstraints().get(6).setMinWidth(70);
+        gpPozycjeZamowienia.getColumnConstraints().get(6).setHalignment(HPos.CENTER);
+
+        gpPozycjeZamowienia.getColumnConstraints().get(7).setMinWidth(70);
+        gpPozycjeZamowienia.getColumnConstraints().get(7).setHalignment(HPos.CENTER);
+
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        list.add("1");
-        list.add("12");
-        list.add("123");
+        list.add(new Towar(1,"asdf",19));
+        list.add(new Towar(2,"asdfa",18));
+        list.add(new Towar(3,"asdfb",17));
+        list.add(new Towar(4,"asdfc",16));
+        list.add(new Towar(5,"asdfd",15));
+        list.add(new Towar(6,"asdfq",14));
+        list.add(new Towar(7,"asdfw",13));
+        list.add(new Towar(8,"asdfe",12));
+        list.add(new Towar(9,"asdfr",11));
+
+        fillGridView();
+        setGridViewConstraints();
+        
         closeButton=btnPowrot;
         zamowieniaRepository = new ZamowieniaRepository();
-
     }
 
     @FXML
@@ -85,9 +176,27 @@ public class NoweZamowienieController extends ViewController implements Initiali
         twController.updateView();
     }
 
+    public void czyNiedostepnaNotify(boolean czyNiedostepna)
+    {
+        if(czyNiedostepna) {
+            lbBrakDostepnosci.setVisible(false);
+            btnWyslijZapytanie.setVisible(false);
+        }
+        else
+        {
+            lbBrakDostepnosci.setVisible(true);
+            btnWyslijZapytanie.setVisible(true);
+        }
+    }
+
     @FXML
     private void dodajPozycjeZamowienia()
     {
-     
     }
+
+    @FXML
+    private void dodajPozycje()
+    {
+    }
+
 }
